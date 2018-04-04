b63d9c37c6f4ca885aab1908d294db45138b4159
Durran Jordan <durran@gmail.com>
2018-04-04 11:43:44 -0400

    Setting up structure

---
+++ index.js
@@ -0,0 +1,3 @@
+module.exports = require('./lib/query-autocompleter');
+module.exports.QueryAutoCompleter = require('./lib/query-autocompleter');
+module.exports.StageAutoCompleter = require('./lib/stage-autocompleter');
