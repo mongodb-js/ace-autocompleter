1492614bfe729b8898786b76a016c77752140806
Durran Jordan <durran@gmail.com>
2018-04-04 17:16:48 -0400

    Adding more query autocompleter tests

--- test/query-autocompleter.test.js
+++ test/query-autocompleter.test.js
@@ -1,6 +1,6 @@
 const { EditSession } = require('brace');
 const ace = require('brace');
-const { QueryAutoCompleter } = require('../');
+const { QueryAutoCompleter, QUERY_OPERATORS } = require('../');
 
 require('brace/mode/javascript');
 require('brace/ext/language_tools');
@@ -30,23 +30,135 @@ describe('QueryAutoCompleter', () => {
       });
 
       context('when the string is a $', () => {
+        const completer = new QueryAutoCompleter('3.6.0', textCompleter, fields);
+        const session = new EditSession('{ $ }', new Mode());
+        const position = { row: 0, column: 2 };
 
+        it('returns all the query operators', () => {
+          completer.getCompletions(editor, session, position, '$', (error, results) => {
+            expect(error).to.equal(null);
+            expect(results).to.deep.equal(QUERY_OPERATORS);
+          });
+        });
       });
 
       context('when the string is $a', () => {
+        const completer = new QueryAutoCompleter('3.6.0', textCompleter, fields);
+        const session = new EditSession('{ $a }', new Mode());
+        const position = { row: 0, column: 3 };
 
+        it('returns all the matching query operators', () => {
+          completer.getCompletions(editor, session, position, '$a', (error, results) => {
+            expect(error).to.equal(null);
+            expect(results).to.deep.equal([
+              {
+                name: '$all',
+                value: '$all',
+                score: 1,
+                meta: 'query',
+                version: '2.2.0'
+              },
+              {
+                name: '$and',
+                value: '$and',
+                score: 1,
+                meta: 'query',
+                version: '2.2.0'
+              }
+            ]);
+          });
+        });
       });
 
       context('when the string matches a bson type', () => {
+        const completer = new QueryAutoCompleter('3.6.0', textCompleter, fields);
+        const session = new EditSession('{ N }', new Mode());
+        const position = { row: 0, column: 2 };
 
+        it('returns all the matching query operators', () => {
+          completer.getCompletions(editor, session, position, 'N', (error, results) => {
+            expect(error).to.equal(null);
+            expect(results).to.deep.equal([
+              {
+                name: 'NumberInt',
+                value: 'NumberInt',
+                label: 'NumberInt',
+                score: 1,
+                meta: 'bson',
+                version: '0.0.0',
+                description: 'BSON 32 bit Integer type',
+                snippet: 'NumberInt(${1:value})'
+              },
+              {
+                name: 'NumberLong',
+                value: 'NumberLong',
+                label: 'NumberLong',
+                score: 1,
+                meta: 'bson',
+                version: '0.0.0',
+                description: 'BSON 64 but Integer type',
+                snippet: 'NumberLong(${1:value})'
+              },
+              {
+                name: 'NumberDecimal',
+                value: 'NumberDecimal',
+                label: 'NumberDecimal',
+                score: 1,
+                meta: 'bson',
+                version: '3.4.0',
+                description: 'BSON Decimal128 type',
+                snippet: "NumberDecimal('${1:value}')"
+              }
+            ]);
+          });
+        });
       });
 
       context('when the version doesnt match all operators', () => {
+        const completer = new QueryAutoCompleter('3.4.0', textCompleter, fields);
+        const session = new EditSession('{ $ }', new Mode());
+        const position = { row: 0, column: 2 };
 
+        it('returns all the query operators', () => {
+          completer.getCompletions(editor, session, position, '$', (error, results) => {
+            expect(error).to.equal(null);
+            expect(results.length).to.equal(QUERY_OPERATORS.length - 2);
+          });
+        });
       });
 
       context('when the version doesnt match a bson type', () => {
+        const completer = new QueryAutoCompleter('3.2.0', textCompleter, fields);
+        const session = new EditSession('{ N }', new Mode());
+        const position = { row: 0, column: 2 };
 
+        it('returns all the query operators', () => {
+          completer.getCompletions(editor, session, position, 'N', (error, results) => {
+            expect(error).to.equal(null);
+            expect(results).to.deep.equal([
+              {
+                name: 'NumberInt',
+                value: 'NumberInt',
+                label: 'NumberInt',
+                score: 1,
+                meta: 'bson',
+                version: '0.0.0',
+                description: 'BSON 32 bit Integer type',
+                snippet: 'NumberInt(${1:value})'
+              },
+              {
+                name: 'NumberLong',
+                value: 'NumberLong',
+                label: 'NumberLong',
+                score: 1,
+                meta: 'bson',
+                version: '0.0.0',
+                description: 'BSON 64 but Integer type',
+                snippet: 'NumberLong(${1:value})'
+              }
+            ]);
+          });
+        });
       });
     });
   });
