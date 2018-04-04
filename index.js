e9a9c91277e968ef06219c4bd9b1c8b5221e2906
Durran Jordan <durran@gmail.com>
2018-04-04 12:51:40 -0400

    Tests passing on stage autocompleter

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 module.exports = require('./lib/query-autocompleter');
 module.exports.QueryAutoCompleter = require('./lib/query-autocompleter');
 module.exports.StageAutoCompleter = require('./lib/stage-autocompleter');
+module.exports.ACCUMULATORS = require('./lib/constants/accumulators');
+module.exports.BSON_TYPES = require('./lib/constants/bson-types');
+module.exports.EXPRESSION_OPERATORS = require('./lib/constants/expression-operators');
+module.exports.QUERY_OPERATORS = require('./lib/constants/query-operators');
+module.exports.STAGE_OPERATORS = require('./lib/constants/stage-operators');
---
+++ test/setup.js
@@ -0,0 +1,33 @@
+const chai = require('chai');
+const sinonChai = require('sinon-chai');
+const jsdom = require('jsdom');
+const virtualConsole = new jsdom.VirtualConsole();
+
+virtualConsole.sendTo(console, { omitJSDOMErrors: true });
+
+require('jsdom-global')('', {
+  virtualConsole: virtualConsole,
+  beforeParse(win) {
+    win.URL = {
+      createObjectURL: () => {}
+    };
+  },
+  runScripts: 'dangerously'
+});
+
+global.sinon = require('sinon');
+global.expect = chai.expect;
+
+class Worker {
+  postMessage() {
+    return true;
+  }
+  terminate() {
+    return true;
+  }
+}
+
+global.Worker = Worker;
+
+chai.should();
+chai.use(sinonChai);
