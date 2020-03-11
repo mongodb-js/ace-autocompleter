21f5518eeb78c597ca17463e98e58d5e67a6832a
Durran Jordan <durran@gmail.com>
2020-03-11 11:44:06 +0100

    Adding supported envs per stage operator

--- index.js
+++ index.js
@@ -10,3 +10,15 @@ module.exports.QUERY_OPERATORS = require('./lib/constants/query-operators');
 module.exports.STAGE_OPERATORS = require('./lib/constants/stage-operators');
 module.exports.JSON_SCHEMA = require('./lib/constants/json-schema');
 module.exports.BSON_TYPE_ALIASES = require('./lib/constants/bson-type-aliases');
+
+const {
+  ATLAS,
+  ADL,
+  ON_PREM,
+  ENVS
+} = require('./lib/constants/env');
+
+module.exports.ATLAS = ATLAS;
+module.exports.ADL = ADL;
+module.exports.ON_PREM = ON_PREM;
+module.exports.ENVS = ENVS;
